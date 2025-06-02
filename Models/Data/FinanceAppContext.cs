72abb431d3069d774b0544151d39bf00c2056902
Muhammad Anas <62480080+Anas-coder7694@users.noreply.github.com>
2025-06-03 01:32:44 +0500

    Add files via upload

---
+++ Models/Data/FinanceAppContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FinanceApp.Models.Data
+{
+    public class FinanceAppContext : DbContext
+    {
+        public FinanceAppContext(DbContextOptions<FinanceAppContext> options):base (options) { }
+
+        DbSet<Expense> Expenses { get; set; }
+    }
+}
